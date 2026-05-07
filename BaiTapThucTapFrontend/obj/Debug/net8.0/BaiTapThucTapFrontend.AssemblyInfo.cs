9899ae57f3af2f15e5f2db79a0d6ad2e44d2d667
nguyentrungtinh <trungtinhn300@gmail.com>
2026-05-07 08:21:37 +0700

    Create page print nhapkho and update layout

--- BaiTapThucTapFrontend/obj/Debug/net8.0/BaiTapThucTapFrontend.AssemblyInfo.cs
+++ BaiTapThucTapFrontend/obj/Debug/net8.0/BaiTapThucTapFrontend.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BaiTapThucTapFrontend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e80ad446984f1f57aec35761429d99f60cad08fb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d093da91ca37bdeca5e3dee1908b0b1962c1f927")]
 [assembly: System.Reflection.AssemblyProductAttribute("BaiTapThucTapFrontend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BaiTapThucTapFrontend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
